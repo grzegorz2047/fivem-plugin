5c77fcb16298135c96935413fd20963897f116ca
grzegorz2047 <grzegorz2047@gmail.com>
2021-04-03 17:05:34 +0200

    pseudo prison

--- minigame-psuedo-prison/client.lua
+++ minigame-psuedo-prison/client.lua
@@ -3,7 +3,8 @@ Citizen.CreateThread(
     function()
         while true do
             Citizen.Wait(1)
-            if (GetDistanceBetweenCoords(GetEntityCoords(playerPed), Config.CircleZones.WeedProcessing.coords, false) > maxDistance) then
+            local playerPed = PlayerPedId()
+            if (GetDistanceBetweenCoords(GetEntityCoords(playerPed), vector3(2395.096, 3049.616, 60.053), false) > maxDistance) then
                 TriggerEvent("chatMessage", "SYSTEM", { 255,0,0}, "PRZEKROCZENIE!")
             end
         end
