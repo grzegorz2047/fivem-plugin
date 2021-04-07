d51a657fb6ba82342fd7ba58fb0ad17b8f9780f6
grzegorz2047 <grzegorz2047@gmail.com>
2021-04-07 23:29:57 +0200

    tests

--- minigame-killing-events/server.lua
+++ minigame-killing-events/server.lua
@@ -1,17 +1,14 @@
--- https://forum.cfx.re/t/how-to-json-with-fivem/237542 
-function isPlayer(source)
-	return source > 0
-end
-
-RegisterCommand("tep", function(source --[[ this is the player ID (on the server): a number ]], args --[[ this is a table of the arguments provided ]], rawCommand --[[ this is what the user entered ]])
-    if not isPlayer(source) then
-		print("komenda tylko dla gracza!")
-		return false
+AddEventHandler('baseevents:onPlayerDied', 
+	function(player, data)
+		TriggerEvent("chatMessage", "SYSTEM", { 255,0,0}, "onPlayerDied cl")
 	end
-	 
-	TriggerClientEvent("minigame:teleportEvent", source, args)
-end, false)
- 
-
-
-
+)
+AddEventHandler('baseevents:onPlayerKilled', function(player, data)
+    local killer = GetPlayerByServerId(data.killer)
+    TriggerEvent('chatMessage', '', { 0, 0, 0 }, 'You was killed by ' .. killer.name .. '.')
+end)
+AddEventHandler('baseevents:onPlayerWasted', 
+	function(player, data)
+		TriggerEvent("chatMessage", "SYSTEM", { 255,0,0}, "onPlayerWasted cl")
+	end
+)
