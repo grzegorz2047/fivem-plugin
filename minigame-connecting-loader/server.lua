52664e99be7db8718f2fb60cd5b0819d96e54b7a
grzegorz2047 <grzegorz2047@gmail.com>
2021-04-07 23:09:27 +0200

    tests

--- minigame-connecting-loader/server.lua
+++ minigame-connecting-loader/server.lua
@@ -2,34 +2,34 @@ AddEventHandler('gameEventTriggered', function (name, args)
   print('game event ' .. name .. ' (' .. json.encode(args) .. ')')
 end)
 
-local function OnPlayerConnecting(name, setKickReason, deferrals)
-    local player = source
-    local steamIdentifier
-    local identifiers = GetPlayerIdentifiers(player)
-    deferrals.defer()
+-- local function OnPlayerConnecting(name, setKickReason, deferrals)
+--     local player = source
+--     local steamIdentifier
+--     local identifiers = GetPlayerIdentifiers(player)
+--     deferrals.defer()
 
-    -- mandatory wait!
-    Wait(0)
+--     -- mandatory wait!
+--     Wait(0)
 
-    deferrals.update(string.format("Hello %s. Your Steam ID is being checked.", name))
+--     deferrals.update(string.format("Hello %s. Your Steam ID is being checked.", name))
 
-    for _, v in pairs(identifiers) do
-        print(v)
-        if string.find(v, "steam") then
-            steamIdentifier = v
-            break
-        end
-    end
+--     for _, v in pairs(identifiers) do
+--         print(v)
+--         if string.find(v, "steam") then
+--             steamIdentifier = v
+--             break
+--         end
+--     end
 
-    -- mandatory wait!
-    Wait(0)
+--     -- mandatory wait!
+--     Wait(0)
 
-    if not steamIdentifier then
-        deferrals.done("You are not connected to Steam.")
-    else
-        deferrals.done()
-    end
-end
+--     if not steamIdentifier then
+--         deferrals.done("You are not connected to Steam.")
+--     else
+--         deferrals.done()
+--     end
+-- end
 
 AddEventHandler("playerConnecting", OnPlayerConnecting)
 
