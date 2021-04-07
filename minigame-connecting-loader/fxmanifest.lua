35d01592cd3d53357a0f8260e4336bd05a507f4b
grzegorz2047 <grzegorz2047@gmail.com>
2021-04-07 22:46:22 +0200

    tests

--- minigame-connecting-loader/fxmanifest.lua
+++ minigame-connecting-loader/fxmanifest.lua
@@ -6,9 +6,22 @@ author 'grzegorz2047 <grzegorz2047@gmail.com>'
 fx_version 'bodacious'
 game 'gta5'
 
- 
+ files {
+    'index.html',
+    'css/style.css',
+    'images/background.jpg',
+    'css/bankgothic.ttf',
+    'loadscreen.jpg',
+	'music/Loading.ogg',
+	'images/logo.png',
+	
+}
+
+loadscreen 'index.html'
+
 client_script 'client.lua'
 server_script 'server.lua'
 
 --dependency 'cfx.re/playerData.v1alpha1'
 lua54 'yes'
+
