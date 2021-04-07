577f949a7e251cd59af3eb2e0695497d68ab6218
grzegorz2047 <grzegorz2047@gmail.com>
2021-04-07 22:42:55 +0200

    tests

---
+++ minigame-connecting-loader/fxmanifest.lua
@@ -0,0 +1,14 @@
+version '1.0.0'
+description 'loader dla minigry'
+repository 'https://github.com/grzegorz2047/fivem-plugin'
+author 'grzegorz2047 <grzegorz2047@gmail.com>'
+
+fx_version 'bodacious'
+game 'gta5'
+
+ 
+client_script 'client.lua'
+server_script 'server.lua'
+
+--dependency 'cfx.re/playerData.v1alpha1'
+lua54 'yes'
---
+++ minigame-connecting-loader/server.lua
@@ -0,0 +1,33 @@
+AddEventHandler('gameEventTriggered', function (name, args)
+  print('game event ' .. name .. ' (' .. json.encode(args) .. ')')
+end)
+
+local function OnPlayerConnecting(name, setKickReason, deferrals)
+    local player = source
+    local steamIdentifier
+    local identifiers = GetPlayerIdentifiers(player)
+    deferrals.defer()
+
+    -- mandatory wait!
+    Wait(0)
+
+    deferrals.update(string.format("Hello %s. Your Steam ID is being checked.", name))
+
+    for _, v in pairs(identifiers) do
+        if string.find(v, "steam") then
+            steamIdentifier = v
+            break
+        end
+    end
+
+    -- mandatory wait!
+    Wait(0)
+
+    if not steamIdentifier then
+        deferrals.done("You are not connected to Steam.")
+    else
+        deferrals.done()
+    end
+end
+
+AddEventHandler("playerConnecting", OnPlayerConnecting)
