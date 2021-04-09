25433fbb04704104286d4f3d7b28e307d9cbfd8c
grzegorz2047 <grzegorz2047@gmail.com>
2021-04-09 15:09:50 +0200

    map

--- fivem-map-prison/map.lua
+++ fivem-map-prison/map.lua
@@ -1,5 +1,6 @@
 vehicle_generator "airtug" { -54.26639938354492, -1679.548828125, 28.4414, heading = 228.2736053466797 }
 
+spawnpoint 'a_m_y_hipster_01' { x = -802.311, y = 175.056, z = 72.8446 }
 spawnpoint 's_m_y_prisoner_01' { x = -1972.0, y = 2573.0, z = 2.0 }
 spawnpoint 's_m_y_prismuscl_01' { x = -1972.0, y = 2573.0, z = 2.0 }
 --
