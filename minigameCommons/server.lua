04517383da9323011ec8a0a9301f4256cc25c8c1
grzegorz2047 <grzegorz2047@gmail.com>
2021-04-09 10:43:36 +0200

    json lua

--- minigameCommons/server.lua
+++ minigameCommons/server.lua
@@ -6,7 +6,7 @@ exports('saveJson', function(filePath, data)
 	print("Zapisano!")
 end)
 
-exports('readJson', function(filePath)
+exports('loadJson', function(filePath)
 	local loadFile = LoadResourceFile(GetCurrentResourceName(), filePath) -- you only have to do this once in your code, i just put it in since it wont get confusing.
 	local extract = {}
 	return json.decode(loadFile)
