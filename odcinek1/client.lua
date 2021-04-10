5a56b9db096df48b6487be2ed51d4da413993ff5
grzegorz2047 <grzegorz2047@gmail.com>
2021-04-10 16:51:18 +0200

    odcinek1

--- odcinek1/client.lua
+++ odcinek1/client.lua
@@ -1,12 +1,12 @@
--- minigun = GetHashKey("WEAPON_MINIGUN")
+minigun = GetHashKey("WEAPON_MINIGUN")
 Citizen.CreateThread(
     function()
         while true do
             Citizen.Wait(0)
-           -- if IsControlJustPressed(1, 137) then
-           --     local playerPed = PlayerPedId()
-           --     GiveWeaponToPed(playerPed, minigun, 10, false)
-          --  end
+            if IsControlJustPressed(1, 137) then
+                local playerPed = PlayerPedId()
+                GiveWeaponToPed(playerPed, minigun, 10, false)
+            end
         end
     end
 )
