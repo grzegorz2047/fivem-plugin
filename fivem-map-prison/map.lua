fe8ec9578525dc3feac77f3af81c45d5aee10e9e
grzegorz2047 <grzegorz2047@gmail.com>
2021-04-10 14:26:49 +0200

    diff models & wanted lvl

--- fivem-map-prison/map.lua
+++ fivem-map-prison/map.lua
@@ -1,5 +1,10 @@
 vehicle_generator "airtug" { -54.26639938354492, -1679.548828125, 28.4414, heading = 228.2736053466797 }
 
+spawnpoint 's_m_y_cop_01' { x = 1711.0, y = 2564.0, z = 45.0 }
+spawnpoint 's_m_y_hwaycop_01' { x = 1711.0, y = 2564.0, z = 45.0 }
+spawnpoint 'csb_cop' { x = 1711.0, y = 2564.0, z = 45.0 }
+spawnpoint 's_f_y_cop_01' { x = 1711.0, y = 2564.0, z = 45.0 }
+spawnpoint 's_m_y_prismuscl_01' { x = 1711.0, y = 2564.0, z = 45.0 }
 spawnpoint 's_m_y_prisoner_01' { x = 1711.0, y = 2564.0, z = 45.0 }
 spawnpoint 's_m_y_prismuscl_01' { x = 1711.0, y = 2564.0, z = 45.0 }
 --
