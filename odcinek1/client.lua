1761fc6d957777fe34c5ad5427a04ecb76fb9dc3
grzegorz2047 <grzegorz2047@gmail.com>
2021-04-10 17:12:08 +0200

    odcinek1

--- odcinek1/client.lua
+++ odcinek1/client.lua
@@ -7,12 +7,18 @@ local function givePlayerWeapon(weaponHash, amount)
     GiveWeaponToPed(playerPed, weaponHash, amount, false)
 end
 
+local function sendMessage(text)
+    TriggerEvent("chatMessage", "SYSTEM", { 255,0,0}, text)
+end
+
+
 local function giveWeaponOnPressed()
     local minigun = GetHashKey("WEAPON_MINIGUN")
     while true do
         Citizen.Wait(0)
         if isCapsLockPressed() then
             givePlayerWeapon(minigun, 10)
+            sendMessage("Otrzymales miniguna!")
         end
     end
 end
