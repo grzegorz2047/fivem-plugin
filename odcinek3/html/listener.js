11a7008b9bfcc3bc0844a137abc93bed32af6b3c
grzegorz2047 <grzegorz2047@gmail.com>
2021-04-12 17:30:35 +0200

    odcinek3

--- odcinek3/html/listener.js
+++ odcinek3/html/listener.js
@@ -2,9 +2,11 @@ $(function(){
 	window.onload = (e) => {
         
 		window.addEventListener('message', (event) => {
-            alert(event.data);
 			var item = event.data;
+			alert(event.data.type);
 			if (item !== undefined && item.type === "UI") {
+				alert(event.data.display);
+				alert(event.data.color);
 				 $("#top-left-square").text = item.display;
 				 $("#bottom-right-square").css("background-color", item.color); 
 			}
