adc137fe5698f0bb82c4d4ee0a72cc58517cd955
grzegorz2047 <grzegorz2047@gmail.com>
2021-04-12 17:25:14 +0200

    odcinek3

--- odcinek3/client.lua
+++ odcinek3/client.lua
@@ -1,29 +1,31 @@
 local displayVal = "Tekst tutka"
 local colorVal = "green"
-Citizen.CreateThread(
-	while true do
-		function()
-			SendNUIMessage({
-				type = "UI",
-				display = displayVal,
-				color = colorVal
-			})
-		end
-	end
-)
+SendNUIMessage({
+	type = "UI",
+	display = displayVal,
+	color = colorVal
+})
 Citizen.CreateThread(
 	function()
 		while true do
 			Citizen.Wait(0)
-			local ped = PlayerPedId()
  			if IsControlJustPressed(1, 323) then
 				displayVal = "Tekst dutka"
 				colorVal = "red"
+				SendNUIMessage({
+					type = "UI",
+					display = displayVal,
+					color = colorVal
+				})
 			end
-
 			if IsControlJustReleased(1, 323) then
 				displayVal = "Tekst tutka"
 				colorVal = "green"
+				SendNUIMessage({
+					type = "UI",
+					display = displayVal,
+					color = colorVal
+				})
 			end
 		end
 	end
--- odcinek3/html/listener.js
+++ odcinek3/html/listener.js
@@ -5,7 +5,6 @@ $(function(){
             
 			var item = event.data;
 			if (item !== undefined && item.type === "UI") {
-             
 				 $("#top-left-square").text = item.display;
 				 $("#bottom-right-square").css("background-color", item.color); 
 			}
