7db635da3fac941f2da610c8175aa93f49eb4b49
grzegorz2047 <grzegorz2047@gmail.com>
2021-04-22 13:11:00 +0200

    odcinek6

---
+++ odcinek6/client.lua
@@ -0,0 +1,15 @@
+local pointOnMap = vector3(1703.0, 2507.0, 45.0)
+local function(pointOnMap, markerType) 
+	DrawMarker(0, pointOnMap.x, pointOnMap.y, pointOnMap.z, 0.0, 0.0, 0.0, 0, 0.0, 0.0, 0.75, 0.75, 0.75, 204, 204, markerType, 100, false, true, 2, false, false, false, false)
+
+end 
+ 
+Citizen.CreateThread(
+    function()
+        while true do
+            Citizen.Wait(0)
+            drawMarker(pointOnMap, 43)
+        end
+    end
+)
+ 
