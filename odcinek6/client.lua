e6fab81d1ea451c327863a754bac69e7911f06ca
grzegorz2047 <grzegorz2047@gmail.com>
2021-04-22 14:30:12 +0200

    odcinek6

--- odcinek6/client.lua
+++ odcinek6/client.lua
@@ -1,7 +1,7 @@
 local pointOnMap = vector3(1703.0, 2507.0, 46.0)
 local areaSize = 2.0
 local function isClose()
-	
+local showInfo = false
 
 end
 milis = 1
@@ -10,13 +10,19 @@ Citizen.CreateThread(
         while true do
             Citizen.Wait(0)
             exports.libCommons:drawMarker(pointOnMap, 28, areaSize)
+			if showInfo then
+				exports.libCommons:nativeMessage("Wciśnij ~INPUT_CELLPHONE_CAMERA_EXPRESSION~ aby wykonać akcję!")
+			end
 			local playerPed = PlayerPedId()
              --warto przeniesc logike na serwer
 			local playerCoords = GetEntityCoords(playerPed)
 			if milis % 100 == 0 then
 				local currentDistance = GetDistanceBetweenCoords(playerCoords, pointOnMap, false)
 				if currentDistance < areaSize then
-					exports.libCommons:nativeMessage("Wciśnij ~INPUT_CELLPHONE_CAMERA_EXPRESSION~ aby wykonać akcję!")
+					showInfo = true
+				end
+				else
+					showInfo = false
 				end
 				milis = 1
 			end
