e214eac3a8456794a8dd475a96c2ee32d75c72ed
grzegorz2047 <grzegorz2047@gmail.com>
2021-04-22 13:37:22 +0200

    odcinek6

--- odcinek6/client.lua
+++ odcinek6/client.lua
@@ -1,6 +1,6 @@
 local pointOnMap = vector3(1703.0, 2507.0, 45.0)
 local function drawMarker(pointOnMap, markerType, size) 
-	DrawMarker(markerType, pointOnMap.x, pointOnMap.y, pointOnMap.z, 0.0, 0.0, 0.0, 0.0, size, 0.0, 0.75, 0.75, 0.75, 204, 204, 0, 100, false, true, 2, false, false, false, false)
+	DrawMarker(markerType, pointOnMap.x, pointOnMap.y, pointOnMap.z, 0.0, 0.0, 0.0, size, size, size, 0.75, 1.75, 0.75, 204, 204, 0, 100, false, true, 2, false, false, false, false)
 end 
  
 Citizen.CreateThread(
