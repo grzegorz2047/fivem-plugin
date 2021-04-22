775c162c8d25cb85f2e8ab9dea694b0df5d94288
grzegorz2047 <grzegorz2047@gmail.com>
2021-04-22 13:25:21 +0200

    odcinek6

--- odcinek6/client.lua
+++ odcinek6/client.lua
@@ -1,5 +1,5 @@
 local pointOnMap = vector3(1703.0, 2507.0, 45.0)
-local function (pointOnMap, markerType) 
+local function drawMarker(pointOnMap, markerType) 
 	DrawMarker(markerType, pointOnMap.x, pointOnMap.y, pointOnMap.z, 0.0, 0.0, 0.0, 0, 0.0, 0.0, 0.75, 0.75, 0.75, 204, 204, 0, 100, false, true, 2, false, false, false, false)
 end 
  
