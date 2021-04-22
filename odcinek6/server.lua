78052a01aa539bc66f04feabbc372dcc0a0f74b4
grzegorz2047 <grzegorz2047@gmail.com>
2021-04-22 14:24:07 +0200

    odcinek6

--- odcinek6/server.lua
+++ odcinek6/server.lua
@@ -1,8 +1,6 @@
 local pointOnMap = vector3(1703.0, 2507.0, 46.0)
-local function ShowCoordinates()
-    local player = source
-    local ped = GetPlayerPed(player)
-    local playerCoords = GetEntityCoords(ped)
+local function ShowCoordinates(playerPed)
+    local playerCoords = GetEntityCoords(playerPed)
 
     print(playerCoords) -- vector3(...)
 end
