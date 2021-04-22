0ff38e17877191669f90d55f0becf74a6be98be9
grzegorz2047 <grzegorz2047@gmail.com>
2021-04-22 10:07:39 +0200

    odcinek4 move

---
+++ odcinek5/client.lua
@@ -0,0 +1,25 @@
+RegisterNetEvent("tut:spawnEvent")
+
+local function sendMessage(text)
+    TriggerEvent("chatMessage", "SYSTEM", { 255,0,0}, text)
+end
+
+local function teleport(playerPed, x,y, z)
+    SetEntityCoords(playerPed, x, y, z, false, false, false, true)
+end
+local function teleportWithVehicle(playerPed, x,y, z)
+    SetPedCoordsKeepVehicle(playerPed, x, y, z)
+end
+
+
+AddEventHandler('tut:spawnEvent', 
+	function(args)
+		local playerPed = PlayerPedId()
+        --SetPedCoordsKeepVehicle(playerPed, x, y, z)
+		local x = -2573.0
+        local y = 2313.0
+        local z = 33.0
+        teleport(playerPed, x, y, z)
+		sendMessage("Przeteleportowano na spawn! " .. args)
+	end
+)
