8864321acf2b5db4d5f92a06e1241801de5a462e
grzegorz2047 <grzegorz2047@gmail.com>
2021-04-25 15:17:04 +0200

    prison

--- minigame-pseudo-prison/server.lua
+++ minigame-pseudo-prison/server.lua
@@ -6,12 +6,14 @@ local function ShowCoordinates(source)
 end
 Citizen.CreateThread(function()
     while true do
-        for _, playerId in ipairs(GetPlayers()) do
-            local name = GetPlayerName(playerId)
-            print(('Player %s with id %i is in the server'):format(name, playerId))
-            ShowCoordinates(playerId)
-          end
-          
+        local players = GetNumPlayerIndices()
+        for i = 0, players - 1 do
+            local id = GetPlayerFromIndex(i)
+            local ped = GetPlayerPed(id)
+            local coords = GetEntityCoords(ped)
+
+            print(id, ped, coords)
+        end
       Citizen.Wait(1000)
     end
   end)
